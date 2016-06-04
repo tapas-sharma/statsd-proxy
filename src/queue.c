3dd3fc6bfa2444391e9001052eabbaf1a5e03fba
Tapas Sharma <tapas.bits@gmail.com>
2016-06-04 14:17:08 +0530

    Removed some dead code, and added minor comments

--- src/queue.c
+++ src/queue.c
@@ -18,29 +18,9 @@
 
 extern confStruct configuration;
 
-
-void free_request_struct(Request *node)
-{
-    Request *tmp;
-    while(node != NULL)
-    {
-        if(node->req_data != NULL)
-            free(node->req_data);
-        if(node->key != NULL)
-            free(node->key);
-        if(node->value != NULL)
-            free(node->value);
-        node->req_data = NULL;
-        node->key = NULL;
-        node->value = NULL;
-        node->process_type_request = NULL;
-        tmp = node;
-        node = node->next;
-        free(tmp);
-    }
-    return;
-}
-
+/**
+ * Free the Queue node and the memory its holding
+ */
 void free_queue_struct(Queue *node)
 {
     if(node->data != NULL)
@@ -48,13 +28,14 @@ void free_queue_struct(Queue *node)
     node->len = 0;
     node->data = NULL;
     memcpy(&node->request_id, "0xFFFFFFFFFFFFFFFF", sizeof(uint32_t));
-    //free_request_struct(node->req);
     node->next=NULL;
     free(node);
     return;
 }
 
-
+/**
+ * Enqueue data in the global request queue
+ */
 ret_val enqueue(Queue **user_q, char *data, int len)
 {
     Queue *head = *user_q, *tmp=NULL;
@@ -86,7 +67,10 @@ ret_val enqueue(Queue **user_q, char *data, int len)
     return TRUE;
 }
 
-
+/**
+ * Always deqeue data from the head of the queue and start processing
+ * more like a FIFO queue, since we are in a UDP based protocol system
+ */
 Queue *dequeue(Queue **user_q)
 {
     Queue *head = *user_q, *tmp;
@@ -100,25 +84,3 @@ Queue *dequeue(Queue **user_q)
     *user_q = head;
     return tmp;
 }
-
-
-/* ret_val insert_in_req(Request **req, char *key, char *value, char type) */
-/* { */
-/*   Request *head = *req; */
-/*   Request *tmp = (Request *)malloc(sizeof(Request)); */
-/*   tmp->key = (char *)malloc(strlen(key)); */
-/*   snprintf(tmp->key, strlen(key), "%s", key); */
-/*   tmp->value = (char *)malloc(strlen(value)); */
-/*   snprintf(tmp->value, strlen(value), "%s", value); */
-/*   tmp->type = type; */
-/*   tmp->next = head; */
-
-/*   if(head == NULL) */
-/*     { */
-/*       *req = tmp; */
-/*     } */
-/*   else */
-/*     { */
-      
-/*     } */
-/* } */
